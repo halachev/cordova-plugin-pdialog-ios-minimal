3f0b42f47a822e8214a4820b7650718177b0fd8a
halachev <bgjoin@gmail.com>
2025-11-23 19:17:24 +0200

    init 4

--- src/ios/PDialog.m
+++ src/ios/PDialog.m
@@ -1,34 +1,73 @@
 #import "PDialog.h"
 
 @interface PDialog ()
-@property (nonatomic, strong) UIActivityIndicatorView *spinner;
 @property (nonatomic, strong) UIView *backgroundView;
+@property (nonatomic, strong) UIVisualEffectView *blurView;
+@property (nonatomic, strong) CAShapeLayer *circularSpinner;
 @end
 
 @implementation PDialog
 
 - (void)init:(CDVInvokedUrlCommand*)command {
     dispatch_async(dispatch_get_main_queue(), ^{
-        if (self.spinner != nil) {
-            [self.spinner stopAnimating];
+        // Премахваме стария spinner
+        if (self.circularSpinner != nil) {
+            [self.circularSpinner removeFromSuperlayer];
+            [self.blurView removeFromSuperview];
             [self.backgroundView removeFromSuperview];
-            self.spinner = nil;
+            self.circularSpinner = nil;
         }
 
         UIView *rootView = self.viewController.view;
 
+        // Background view
         self.backgroundView = [[UIView alloc] initWithFrame:rootView.bounds];
-        self.backgroundView.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.3];
+        self.backgroundView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
 
-        self.spinner = [[UIActivityIndicatorView alloc]
-                        initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleLarge];
-        self.spinner.center = rootView.center;
+        // Винаги тъмен blur
+        UIBlurEffect *blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleDark];
+        self.blurView = [[UIVisualEffectView alloc] initWithEffect:blurEffect];
+        self.blurView.frame = self.backgroundView.bounds;
+        self.blurView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
 
-        [self.backgroundView addSubview:self.spinner];
+        // Много прозрачен overlay
+        self.blurView.backgroundColor = [[UIColor colorWithWhite:0.0 alpha:0.05] colorWithAlphaComponent:0.05];
+
+        [self.backgroundView addSubview:self.blurView];
         [rootView addSubview:self.backgroundView];
 
-        [self.spinner startAnimating];
+        // Circular spinner (винаги бял)
+        CGFloat spinnerSize = 50.0;
+        CAShapeLayer *circleLayer = [CAShapeLayer layer];
+        circleLayer.strokeColor = [UIColor whiteColor].CGColor;
+        circleLayer.fillColor = UIColor.clearColor.CGColor;
+        circleLayer.lineWidth = 4.0;
+        circleLayer.lineCap = kCALineCapRound;
 
+        CGFloat radius = spinnerSize / 2.0;
+        UIBezierPath *path = [UIBezierPath bezierPathWithArcCenter:CGPointMake(radius, radius)
+                                                            radius:radius - 2
+                                                        startAngle:0
+                                                          endAngle:M_PI * 1.5
+                                                         clockwise:YES];
+        circleLayer.path = path.CGPath;
+
+        // Positioning
+        circleLayer.frame = CGRectMake((rootView.bounds.size.width - spinnerSize)/2.0,
+                                       (rootView.bounds.size.height - spinnerSize)/2.0,
+                                       spinnerSize, spinnerSize);
+
+        // Rotation animation
+        CABasicAnimation *rotation = [CABasicAnimation animationWithKeyPath:@"transform.rotation"];
+        rotation.byValue = @(2 * M_PI);
+        rotation.duration = 1.0;
+        rotation.repeatCount = INFINITY;
+        [circleLayer addAnimation:rotation forKey:@"rotationAnimation"];
+
+        [self.backgroundView.layer addSublayer:circleLayer];
+        self.circularSpinner = circleLayer;
+
+        // Callback към JS
         CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
         [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
     });
@@ -36,10 +75,11 @@
 
 - (void)dismiss:(CDVInvokedUrlCommand*)command {
     dispatch_async(dispatch_get_main_queue(), ^{
-        if (self.spinner != nil) {
-            [self.spinner stopAnimating];
+        if (self.circularSpinner != nil) {
+            [self.circularSpinner removeFromSuperlayer];
+            [self.blurView removeFromSuperview];
             [self.backgroundView removeFromSuperview];
-            self.spinner = nil;
+            self.circularSpinner = nil;
         }
 
         CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
@@ -48,3 +88,4 @@
 }
 
 @end
+
