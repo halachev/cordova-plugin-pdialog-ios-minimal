976b33766164c633ece875521d97e6764a5b025b
halachev <bgjoin@gmail.com>
2025-11-28 19:22:13 +0200

    Update PDialog.h

--- src/ios/PDialog.h
+++ src/ios/PDialog.h
@@ -1,6 +1,13 @@
 #import <Cordova/CDVPlugin.h>
+#import <UIKit/UIKit.h>
 
-@interface PDialog : CDVPlugin
+@interface PDialog : CDVPlugin <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+
+// Оригинални методи
 - (void)init:(CDVInvokedUrlCommand*)command;
 - (void)dismiss:(CDVInvokedUrlCommand*)command;
+
+// Нов метод за отваряне на галерия
+- (void)openGallery:(CDVInvokedUrlCommand*)command;
+
 @end
