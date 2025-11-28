c28bbf058844507c5cbbead6d4ddf55185a8369d
halachev <bgjoin@gmail.com>
2025-11-28 19:21:52 +0200

    Add gallery functionality to PDialog

--- src/ios/PDialog.m
+++ src/ios/PDialog.m
@@ -1,11 +1,18 @@
 #import "PDialog.h"
+#import <Photos/Photos.h>
+#import <UIKit/UIKit.h>
 
-@interface PDialog ()
+@interface PDialog () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 @property (nonatomic, strong) UIView *spinnerContainer;
 @property (nonatomic, strong) CAShapeLayer *circularSpinner;
+
+// Нови свойства за галерия
+@property (nonatomic, strong) CDVInvokedUrlCommand *galleryCommand;
 @end
 
 @implementation PDialog
+
+#pragma mark - Spinner (оригинален код)
 
 - (void)init:(CDVInvokedUrlCommand*)command {
     dispatch_async(dispatch_get_main_queue(), ^{
@@ -77,6 +84,67 @@
         CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
         [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
     });
+}
+
+#pragma mark - Gallery (нови методи)
+
+- (void)openGallery:(CDVInvokedUrlCommand*)command {
+    self.galleryCommand = command;
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+        picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+        picker.delegate = self;
+        picker.allowsEditing = NO;
+        
+        // Премахваме спинъра точно след като галерията се появи
+        [self.viewController presentViewController:picker animated:YES completion:^{
+            if (self.spinnerContainer != nil) {
+                [self.spinnerContainer removeFromSuperview];
+                self.spinnerContainer = nil;
+                self.circularSpinner = nil;
+            }
+        }];
+    });
+}
+
+
+// Delegate метод след избор на снимка
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    __weak PDialog *weakSelf = self; // слаб референс за избягване на retain cycle
+    [picker dismissViewControllerAnimated:YES completion:^{
+        PDialog *strongSelf = weakSelf;
+        if (!strongSelf) return;
+        
+        // Показваме спинъра отново
+        [strongSelf init:nil]; // използваме вече съществуващия метод за спинър
+        
+        NSURL *imageURL = [info valueForKey:UIImagePickerControllerImageURL];
+        
+        if (!imageURL) {
+            // fallback за iOS < 11
+            UIImage *image = info[UIImagePickerControllerOriginalImage];
+            NSString *path = [NSTemporaryDirectory() stringByAppendingPathComponent:@"temp_photo.jpg"];
+            NSData *imageData = UIImageJPEGRepresentation(image, 0.5);
+            [imageData writeToFile:path atomically:YES];
+            imageURL = [NSURL fileURLWithPath:path];
+        }
+        
+        // Връщаме резултата към JS
+        CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:imageURL.path];
+        [strongSelf.commandDelegate sendPluginResult:result callbackId:strongSelf.galleryCommand.callbackId];
+        strongSelf.galleryCommand = nil;
+    }];
+}
+
+
+// Delegate метод при отказ
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    [picker dismissViewControllerAnimated:YES completion:nil];
+    
+    CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"User canceled"];
+    [self.commandDelegate sendPluginResult:result callbackId:self.galleryCommand.callbackId];
+    self.galleryCommand = nil;
 }
 
 @end
