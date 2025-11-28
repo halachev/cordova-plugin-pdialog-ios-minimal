e77114c91cabe712a654fc3467eb4361363788c0
halachev <bgjoin@gmail.com>
2025-11-28 19:22:38 +0200

    Implement openGallery method in pdialog.js
    Added openGallery method for gallery access.

--- www/pdialog.js
+++ www/pdialog.js
@@ -1,9 +1,17 @@
-var exec = require('cordova/exec');
+cordova.define("cordova-plugin-pdialog-ios-minimal.PDialog", function(require, exports, module) {
+    var exec = require('cordova/exec');
 
-exports.init = function () {
-    exec(null, null, "PDialog", "init", []);
-};
+    // Оригинални методи
+    exports.init = function () {
+        exec(null, null, "PDialog", "init", []);
+    };
 
-exports.dismiss = function () {
-    exec(null, null, "PDialog", "dismiss", []);
-};
+    exports.dismiss = function () {
+        exec(null, null, "PDialog", "dismiss", []);
+    };
+
+    // Нов метод за отваряне на галерия
+    exports.openGallery = function(successCallback, errorCallback) {
+        exec(successCallback, errorCallback, "PDialog", "openGallery", []);
+    };
+});
