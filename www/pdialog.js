e33bbedc3f4da0a096398e54f905f00fbaea18a5
meetapp2017 <meetapp2017@gmail.com>
2026-03-22 18:23:42 +0200

    init

--- www/pdialog.js
+++ www/pdialog.js
@@ -1,17 +1,28 @@
 cordova.define("cordova-plugin-pdialog-ios-minimal.PDialog", function(require, exports, module) {
     var exec = require('cordova/exec');
 
-    // Оригинални методи
-    exports.init = function () {
-        exec(null, null, "PDialog", "init", []);
-    };
+    module.exports = {
+        /**
+         * Инициализира PDialog.
+         */
+        init: function() {
+            exec(null, null, "PDialog", "init", []);
+        },
 
-    exports.dismiss = function () {
-        exec(null, null, "PDialog", "dismiss", []);
-    };
+        /**
+         * Затваря текущия PDialog.
+         */
+        dismiss: function() {
+            exec(null, null, "PDialog", "dismiss", []);
+        },
 
-    // Нов метод за отваряне на галерия
-    exports.openGallery = function(successCallback, errorCallback) {
-        exec(successCallback, errorCallback, "PDialog", "openGallery", []);
+        /**
+         * Отваря галерията.
+         * @param {function} successCallback - callback при успех
+         * @param {function} errorCallback - callback при грешка
+         */
+        openGallery: function(successCallback, errorCallback) {
+            exec(successCallback, errorCallback, "PDialog", "openGallery", []);
+        }
     };
 });
